2e1cc1c93d131dd872253ad99f2ee50abc6c0dbb
Gordan Grasarevic <me@ggordan.com>
2016-09-12 18:44:12 +0100

    no background color

--- SwipeCards.js
+++ SwipeCards.js
@@ -23,8 +23,7 @@ var styles = StyleSheet.create({
     container: {
         flex: 1,
         justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#F5FCFF'
+        alignItems: 'center'
     },
     yup: {
         borderColor: 'green',
